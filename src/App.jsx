7bd20e45cf42f3e6114b0662fe475c10b955ee3b
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-06-03 16:38:11 +0200

    add posts list in postspage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import AboutUsPage from "./pages/AboutUsPage";
 import PostsPage from "./pages/PostsPage";
+// import NotFoundPage from "./pages/NotFoundPage";
 
 //layouts
 import DefaultLayout from "./layouts/DefaultLayout";
@@ -15,8 +16,14 @@ function App() {
         {/* Rotte che hanno lo stesso layout */}
         <Route element={<DefaultLayout />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="/posts" element={<PostsPage />} />
+
           <Route path="/aboutus" element={<AboutUsPage />} />
+          {/* Rotte che hanno in comune il prefisso /posts */}
+          <Route path="/posts">
+            <Route path="" element={<PostsPage />} />
+          </Route>
+
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,7 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function PostsPage() {
+  const [posts, setPosts] = useState();
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts").then((res) => {
+      console.log(res.data.data);
+      setPosts(res.data.data);
+    });
+  }, []);
+
   return (
     <div className="container">
       <h1>Lista dei post</h1>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Titolo</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {/* <tr>
+            <th>1</th>
+            <th>Ciao</th>
+            <th>Click</th>
+          </tr> */}
+          {posts.map((post) => {
+            return (
+              <tr key={post.id}>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>Click</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
